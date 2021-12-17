1c4ff59facf12042f7ff772948ae11cd61a040c8
Antonio Marino <marino.antonio375@gmail.com>
2021-12-17 04:20:04 -0500

    pushing to heroku

--- server/server.js
+++ server/server.js
@@ -42,11 +42,6 @@ if (process.env.NODE_ENV === "production") {
 //commented below parts out and brought in unit 21, activity #27 code
 // app.use(routes);
 
-// db.once("open", () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
-
-//unit 21, act. #27 code
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
