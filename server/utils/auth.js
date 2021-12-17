99118b4c5d123eee0a51741d85469f8d53f0bc74
Antonio Marino <marino.antonio375@gmail.com>
2021-12-17 03:55:24 -0500

    still having issues with NPM

---
+++ server/utils/auth.js
@@ -0,0 +1,43 @@
+const jwt = require("jsonwebtoken");
+
+// set token secret and expiration date
+const secret = "mysecretsshhhhh";
+const expiration = "2h";
+
+module.exports = {
+  // function for our authenticated routes
+  authMiddleware: function (req, res, next) {
+    // allows token to be sent via  req.query or headers
+    //Unit 21, Act. #26
+    // add and allow token to be sent to req.body as well
+    let token = req.query.token || req.body.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return res.status(400).json({ message: "You have no token!" });
+    }
+
+    // verify token and get user data out of it
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+      return res.status(400).json({ message: "invalid token!" });
+    }
+
+    // send to next endpoint
+    // next();
+    //Unit #21, Act. #26: return requested object that was updated
+    return req;
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
