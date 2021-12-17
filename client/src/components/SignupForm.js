070d16dc1cefc46bf3e13d04a261db9e58364a00
Antonio Marino <marino.antonio375@gmail.com>
2021-12-17 03:11:29 -0500

    trying to fix folder set up

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,17 +1,13 @@
-// replace addUser functionality with ADD USER mutation functionality
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../utils/mutations';
-// import { createUser } from '../utils/API';
+
+import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
-  // const [validated] = useState(false);
-  const [addUser, { error }] = useMutation(ADD_USER);
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
@@ -32,24 +28,17 @@ const SignupForm = () => {
     }
 
     try {
-      // const response = await createUser(userFormData);
+      const response = await createUser(userFormData);
 
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
 
-      // const { token, user } = await response.json();
-      // console.log(user);
-      // Auth.login(token);
-
-      const { data } = await addUser({
-        variables: { ...userFormData },
-      });
-      console.log(data);
-
-      Auth.login(data.addUser.token);
-    } catch (error) {
-      console.error(error);
+      const { token, user } = await response.json();
+      console.log(user);
+      Auth.login(token);
+    } catch (err) {
+      console.error(err);
       setShowAlert(true);
     }
 
