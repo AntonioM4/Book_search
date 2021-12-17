6c01b850001888a07e26a7cb893e68f2dcd3204c
Antonio Marino <marino.antonio375@gmail.com>
2021-12-17 03:47:11 -0500

    still having issues with NPM install

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,21 @@
-// TODO create an Apollo Provider to make every request work with the Apollo server
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-// import { setContext } from '@apollo/client/link.context';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
-
-// added ApolloClient, added lines 9-13, added <AP> tags before and after Router tags
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-});
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <>
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={SearchBooks} />
-            <Route exact path='/saved' component={SavedBooks} />
-            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-          </Switch>
-        </>
-      </Router>
-    </ApolloProvider>
+    <Router>
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={SearchBooks} />
+          <Route exact path="/saved" component={SavedBooks} />
+          <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+        </Switch>
+      </>
+    </Router>
   );
 }
 
