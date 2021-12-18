23200bbcf8aa2f8c690bbe717f72d21123c203cb
Antonio Marino <marino.antonio375@gmail.com>
2021-12-18 10:07:13 -0500

    trying to fix router issue

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 // testing comment
 console.log("testing comment");
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/BookSearch', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Book_search', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 // const db = require('./config/connection');
 const routes = require("./routes");
+module.exports = router;
 
 //Unit 21, Activity #2:
 // Import the ApolloServer class
