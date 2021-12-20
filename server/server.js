efd2f970012e51ccb86252c80295183420663b2f
Antonio Marino <marino.antonio375@gmail.com>
2021-12-19 23:31:41 -0500

    fixing server code

--- server/server.js
+++ server/server.js
@@ -1,52 +1,47 @@
-const express = require("express");
-const path = require("path");
-// const db = require('./config/connection');
-const routes = require("./routes");
-
-//Unit 21, Activity #2:
-// Import the ApolloServer class
-const { ApolloServer } = require("apollo-server-express");
-
-//Unit 21, Activity #2:
-// Import the two parts of a GraphQL schema
-const { typeDefs, resolvers } = require("./schemas");
-const db = require("./config/connection");
-
-//Updating authMiddlesware
-const { authMiddleware } = require("./utils/auth");
+// TODO implement Apollo server
+const express = require('express');
+const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
+const db = require('./config/connection');
+// const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas')
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// added server below and requirers above
+// TODO errors showing here, figure out why.
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware
+// });
 
-//unit 21, activity #2:
-// Create a new instance of an Apollo server with the GraphQL schema
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  //call authMiddleware
-  context: authMiddleware,
-});
+let server;
+async function startApollo() {
+  server = new ApolloServer({ typeDefs, resolvers, context: authMiddleware, plugins: [ApolloServerPluginLandingPageGraphQLPlayground()], playground: true });
 
-//Unit 21, Activity #2:
-// Update Express.js to use Apollo server features
-server.applyMiddleware({ app });
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  await server.start();
+  server.applyMiddleware({ app });
 }
 
-//commented below parts out and brought in unit 21, activity #27 code
-// app.use(routes);
+startApollo()
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
 
-db.once("open", () => {
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
+db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
